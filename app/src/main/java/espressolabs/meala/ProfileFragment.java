e4dbd0bbbf1ac6691744b68e967e795fda5fd7ea
Hannah <hanna7810@hotmail.com>
2018-07-22 16:26:06 -0400

    update profile

--- app/src/main/java/espressolabs/meala/ProfileFragment.java
+++ app/src/main/java/espressolabs/meala/ProfileFragment.java
@@ -1,5 +1,8 @@
 package espressolabs.meala;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
@@ -8,26 +11,42 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.OvershootInterpolator;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
+import espressolabs.meala.model.StatisticListItem;
+import espressolabs.meala.ui.interaction.ItemAnimator;
+import espressolabs.meala.ui.interaction.ProfileViewAdapter;
+import espressolabs.meala.ui.interaction.StatisticRecyclerViewAdapter;
+
 public class ProfileFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ProfileViewAdapter adapter;
+    private RecyclerView listView;
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    private int mColumnCount = 1;
+    private StatisticFragment.OnStatisticClickListener mListener;
+
+    private static final int STATE_STARTING = -1;
+    private static final int STATE_LOADING = 0;
+    private static final int STATE_EMPTY = 1;
+    private static final int STATE_LIST = 2;
+    private int state = STATE_STARTING;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -38,30 +57,22 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
+    /*public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
         }
     }
 
@@ -69,7 +80,32 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        Context context = getContext();
+
+        // Setup RecyclerView
+        listView = view.findViewById(R.id.profile_list);
+        listView.setLayoutManager(new LinearLayoutManager(context));
+
+        // Setup adapter
+        adapter = new ProfileViewAdapter(mListener, Glide.with(this), mColumnCount);
+        //adapter.setViewSize(prefs.getInt(PREFS_VIEW_SIZE, -1));
+        listView.setAdapter(adapter);
+        listView.setItemAnimator(new ItemAnimator(context, adapter));
+
+        // Load initial data
+        ArrayList<StatisticListItem> items = new ArrayList<>(1);
+        items.add(new StatisticListItem("%",50));
+        items.add(new StatisticListItem("%",90));
+        adapter.setItems(items);
+
+    }
+
+    public interface OnStatisticClickListener {
+        //void onListFragmentInteraction(RecipeContent.Recipe item);
+    }
 }
