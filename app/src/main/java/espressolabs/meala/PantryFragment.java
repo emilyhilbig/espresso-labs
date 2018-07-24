3f074c4cb87484360921478dc9963dc5314a3dd0
EMILY HILBIG <eshilbig@uwaterloo.ca>
2018-07-24 09:46:51 -0400

    added pantry logic

--- app/src/main/java/espressolabs/meala/PantryFragment.java
+++ app/src/main/java/espressolabs/meala/PantryFragment.java
@@ -12,19 +12,21 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PantryFragment extends Fragment {
+public class PantryFragment extends ItemListFragment {
 
+    public static final String TYPE = "Pantry Item";
+    @Override
+    protected String getItemType() { return TYPE;}
 
     public PantryFragment() {
         // Required empty public constructor
     }
 
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pantry, container, false);
-    }
+    //    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_pantry, container, false);
+//    }
 
 }
